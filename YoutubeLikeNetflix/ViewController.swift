dde3fc608a35029ae2f17b7ea4cee3928feda0f1
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-10-30 23:11:12 +0900

    Initial Commit

---
+++ YoutubeLikeNetflix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeLikeNetflix
+//
+//  Created by 鈴木友也 on 2019/10/30.
+//  Copyright © 2019 tomoya.suzuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
