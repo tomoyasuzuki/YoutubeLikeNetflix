4b953e16629a6d811016e31ab092565920225f01
tomoyasuzuki <tomoya.s0121@icloud.com>
2019-10-31 02:08:47 +0900

    Added Xib layout and Autolayout. Constraints of Storyboard  is WIP.

--- YoutubeLikeNetflix/View/Cell/VideoTableViewCell.swift
+++ YoutubeLikeNetflix/View/Cell/VideoTableViewCell.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import WebKit
 
 class VideoTableViewCell: UITableViewCell {
-
+    @IBOutlet weak var videoWebView: WKWebView!
+    @IBOutlet weak var videoStartButton: UIButton!
+    @IBOutlet weak var channelImageView: UIImageView!
+    @IBOutlet weak var videoTitleLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
